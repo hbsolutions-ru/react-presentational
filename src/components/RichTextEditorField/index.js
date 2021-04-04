7966d32b86591b8a0df3d55c617f7e4ff3b55b5b
Igor Losev <igor.losev@gmail.com>
2021-04-04 15:56:56 +0700

    [IMPROVEMENT] Pass customControls prop to the RichTextEditor.

--- src/components/RichTextEditorField/index.js
+++ src/components/RichTextEditorField/index.js
@@ -4,7 +4,7 @@ import RichTextEditor from 'react-rte';
 
 import styles from './RichTextEditorField.module.css';
 
-const RichTextEditorField = ({ autoFocus, className, format, placeholder, readOnly, toolbarConfig, ...props }) => {
+const RichTextEditorField = ({ autoFocus, className, format, placeholder, readOnly, toolbarConfig, customControls, ...props }) => {
     const { setFieldValue } = useFormikContext();
     const [field] = useField(props);
 
@@ -12,7 +12,7 @@ const RichTextEditorField = ({ autoFocus, className, format, placeholder, readOn
 
     return (
         <div className={`${styles["editor-container"]} ${className ? className.toString() : ''}`}>
-            <RichTextEditor {...{autoFocus, placeholder, readOnly, toolbarConfig}}
+            <RichTextEditor {...{autoFocus, placeholder, readOnly, toolbarConfig, customControls}}
                             value={value}
                             onChange={value => setValue(value)}
                             onBlur={() => setFieldValue(field.name, value.toString(format))}
