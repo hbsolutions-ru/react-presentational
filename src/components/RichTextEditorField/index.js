237a6c918dd689f379eaa4b2916350d0d98dc7c7
Igor Losev <igor.losev@gmail.com>
2021-04-05 13:24:05 +0700

    [IMPROVEMENT] RichTextEditorField: pass all props to the RichTextEditor component.

--- src/components/RichTextEditorField/index.js
+++ src/components/RichTextEditorField/index.js
@@ -4,9 +4,9 @@ import RichTextEditor from 'react-rte';
 
 import styles from './RichTextEditorField.module.css';
 
-const RichTextEditorField = ({ autoFocus, className, format, customControlsFactory, placeholder, readOnly, toolbarConfig, ...props }) => {
+const RichTextEditorField = ({ name, format, className, customControlsFactory, ...props }) => {
     const { setFieldValue } = useFormikContext();
-    const [field] = useField(props);
+    const [field] = useField(name);
 
     const [value, setValue] = useState(RichTextEditor.createValueFromString(field.value || '', format));
 
@@ -16,7 +16,8 @@ const RichTextEditorField = ({ autoFocus, className, format, customControlsFacto
 
     return (
         <div className={`${styles["editor-container"]} ${className ? className.toString() : ''}`}>
-            <RichTextEditor {...{autoFocus, placeholder, readOnly, toolbarConfig, customControls}}
+            <RichTextEditor customControls={customControls}
+                            {...props}
                             value={value}
                             onChange={value => setValue(value)}
                             onBlur={() => setFieldValue(field.name, value.toString(format))}
