f8a8f9a4bb38d925a9b7414e13826785f6ef4b88
Igor Losev <igor.losev@gmail.com>
2021-05-20 22:38:26 +0700

    [IMPROVEMENT] Improve appearance of the DropdownCheckboxesPalette.

--- src/components/DropdownCheckboxesPalette/index.js
+++ src/components/DropdownCheckboxesPalette/index.js
@@ -8,7 +8,7 @@ import CheckboxesPalette from '../CheckboxesPalette';
 
 import styles from './DropdownCheckboxesPalette.module.css';
 
-const DropdownCheckboxesPalette = ({ variant, pillVariant, disabled, className, paletteClassName, ...props }) => {
+const DropdownCheckboxesPalette = ({ variant, pillVariant, menuAlign, disabled, className, itemColSize, paletteClassName, ...props }) => {
     const [field] = useField(props);
 
     if (!Array.isArray(field.value)) {
@@ -16,19 +16,24 @@ const DropdownCheckboxesPalette = ({ variant, pillVariant, disabled, className, 
         return '';
     }
 
+    const dropdownSizeStyle = [4, 6, 12].indexOf(parseInt(itemColSize)) !== -1 ? 'dropdown-size-' + itemColSize : 'dropdown-size';
+
     return (
         <Dropdown>
             <Dropdown.Toggle variant={variant} disabled={disabled}
                              className={`${className} dropdown-select`}
             >
                 {field.value.length ? (
-                    <Badge pill variant={pillVariant}>
+                    <Badge pill variant={pillVariant || 'primary'}>
                         {field.value.length} items selected
                     </Badge>
                 ) : 'Any'}
             </Dropdown.Toggle>
-            <Dropdown.Menu className={styles["dropdown"]}>
+            <Dropdown.Menu className={`${styles["dropdown"]} ${styles[dropdownSizeStyle]}`}
+                           align={menuAlign || 'left'}
+            >
                 <CheckboxesPalette {...props}
+                                   itemColSize={itemColSize}
                                    className={`${styles["palette"]} ${paletteClassName ? paletteClassName.toString() : 'bg-light'}`}
                 />
             </Dropdown.Menu>
